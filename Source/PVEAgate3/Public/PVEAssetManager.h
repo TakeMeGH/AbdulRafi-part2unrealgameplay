24ad1b36bac04ccbe9e929bb6018d6c66628ed1b
Abdul Rafi <63101627+TakeMeGH@users.noreply.github.com>
2024-06-24 14:13:13 +0700

    first commit

---
+++ Source/PVEAgate3/Public/PVEAssetManager.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/AssetManager.h"
+#include "PVEAssetManager.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PVEAGATE3_API UPVEAssetManager : public UAssetManager
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void StartInitialLoading() override;
+};
